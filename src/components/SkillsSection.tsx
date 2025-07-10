54934b8841637e1daddcaf39e2a0488adda46338
AbdellahiAhmed <abdellahiahmedahmedbaba@gmail.com>
2025-07-10 21:08:33 +0000

    customise the skills section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -43,9 +43,6 @@ const SkillsSection = () => {
                   className={`absolute top-0 left-0 h-full bg-gradient-to-r from-${gradientFrom}-500 to-${gradientTo}-500 rounded-full transition-all duration-700`}
                   style={{ width: `${level}%` }}
                 />
-                <span className="absolute right-2 top-1/2 -translate-y-1/2 text-xs font-semibold text-gray-700 dark:text-slate-300">
-                  {level}%
-                </span>
               </div>
             </div>
           ))}
