4f76e9681af4ed0bf5921d3f3ba6101652880a7e
Abdellahi <AbdellahiAhmedAhmedBaba@gmail.com>
2026-03-31 18:45:45 +0000

    Customize project card display URLs

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -13,6 +13,8 @@ interface Project {
   demo?: string;
   featured?: boolean;
   type?: 'mobile' | 'web';
+  displayUrl?: string;
+  hideUrl?: boolean;
 }
 
 const ProjectsSection = () => {
@@ -175,9 +177,13 @@ const ProjectsSection = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
               </svg>
               <span className="truncate">
-                {project.github
-                  ? project.github.replace('https://', '').replace('github.com/', '')
-                  : `${project.title.toLowerCase().replace(/\s+/g, '')}.app`}
+                {project.hideUrl
+                  ? ''
+                  : project.displayUrl
+                    ? project.displayUrl.replace(/^https?:\/\//, '').replace(/\/$/, '')
+                    : project.github
+                      ? project.github.replace('https://', '').replace('github.com/', '')
+                      : `${project.title.toLowerCase().replace(/\s+/g, '')}.app`}
               </span>
             </div>
           </div>
--- src/pages/WorksPage.tsx
+++ src/pages/WorksPage.tsx
@@ -11,6 +11,8 @@ interface Project {
   demo?: string;
   featured?: boolean;
   type?: 'mobile' | 'web';
+  displayUrl?: string;
+  hideUrl?: boolean;
 }
 
 const WorksPage = () => {
@@ -151,9 +153,13 @@ const WorksPage = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
               </svg>
               <span className="truncate">
-                {project.github
-                  ? project.github.replace('https://', '').replace('github.com/', '')
-                  : `${project.title.toLowerCase().replace(/\s+/g, '')}.app`}
+                {project.hideUrl
+                  ? ''
+                  : project.displayUrl
+                    ? project.displayUrl.replace(/^https?:\/\//, '').replace(/\/$/, '')
+                    : project.github
+                      ? project.github.replace('https://', '').replace('github.com/', '')
+                      : `${project.title.toLowerCase().replace(/\s+/g, '')}.app`}
               </span>
             </div>
           </div>
